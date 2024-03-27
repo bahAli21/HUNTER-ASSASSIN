e71f21a015261c0a71e8678f2a5ba667d9a96c8c
barryelhadj23 <elhadjamadoubarry17@gmail.com>
2024-03-27 02:06:34 +0100

    ouverture et fermeture des portes

--- map/map.cpp
+++ map/map.cpp
@@ -22,6 +22,13 @@ void Map::makeMap(SDL_Renderer * renderer) {
     for(int i=0; i<58;i++){
         drawObstacle(renderer,imgPaths[i],recTab[i]);
     }
+    //ouverture et fermeture des portes
+    Uint8 time1 = SDL_GetTicks();
+    openDoor();
+    Uint8 time2 = SDL_GetTicks() - time1;
+    if(time2>=1000){
+        closeDoor();
+    }
 }
 
 void Map::initPathMap1(){
@@ -50,6 +57,27 @@ void Map::initPathMap1(){
 
 //la deuxieme map
 void Map::initPathMap2(){
+    pathTexture(imgPaths,"../data/imgMap1/texture.bmp",1); // O moquette
+    pathTexture(imgPaths,"../data/imgMap1/porte.bmp",1); // 1 porte
+    pathTexture(imgPaths,"../data/imgMap1/mur-entree.bmp",2); // 2 3 mur
+    pathTexture(imgPaths,"../data/imgMap1/porte jaune.bmp",6); // portes jaunes 4 5 6 7 8 9
+    pathTexture(imgPaths,"../data/imgMap1/block.bmp",8); // bordure gauche 10, bordure haut 11, bordure droite 12, block 13,  piliers 14 15, block bas 16, block haut 17
+    pathTexture(imgPaths,"../data/imgMap1/bas.bmp",3); // block bas cote gauche 18 19 20
+    pathTexture(imgPaths,"../data/imgMap1/droite.bmp",3); // block droit cote gauche 21 22 23
+    pathTexture(imgPaths,"../data/imgMap1/haut.bmp",3); // block haut cote gauche 24 25 26
+    pathTexture(imgPaths,"../data/imgMap1/gauche.bmp",3); // block gauche coté gauche 27 28 29
+    pathTexture(imgPaths,"../data/imgMap1/portesBlocks.bmp",3); // portes des blocks cote gauche 30 31 32
+    pathTexture(imgPaths,"../data/imgMap1/bas.bmp",3); // block bas cote droit 33 34 35
+    pathTexture(imgPaths,"../data/imgMap1/droite.bmp",3); // block droit cote droit 36 37 38
+    pathTexture(imgPaths,"../data/imgMap1/haut.bmp",3); // block haut cote droit 39 40 41
+    pathTexture(imgPaths,"../data/imgMap1/gauche.bmp",3); // block gauche coté droit 42 43 44
+    pathTexture(imgPaths,"../data/imgMap1/portesBlocks.bmp",3); // portes des blocks cote droit 45 46 47
+    pathTexture(imgPaths,"../data/imgMap1/block_H.bmp",2); // 48 50 obstacle gauche droite horizontal
+    pathTexture(imgPaths,"../data/imgMap1/block_V.bmp",3); // 49 51 52 obstacle gauche droite vertical
+    pathTexture(imgPaths,"../data/imgMap1/block_H.bmp",2); // 53 54 obstacle gauche horizontal
+    pathTexture(imgPaths,"../data/imgMap1/clef.bmp",1); // 55 clef
+    pathTexture(imgPaths,"../data/imgMap1/diamant.bmp",1); // 56 diamant
+    pathTexture(imgPaths,"../data/imgMap1/block_Carre.bmp",1); // 57 block carre
 }
 //un probleme de concatenation à resoudre
 void Map::initAllRectangle() {
@@ -69,11 +97,38 @@ void Map::initAllRectangle() {
     //chemin
     initPathMap1();
     //initPathMap2();
-}
 
+}
 
 void Map::pathTexture(vector<char *> &imgPaths, char *image, int repetition) {
     for(int i=1; i<=repetition; i++){
         imgPaths.push_back(image);
     }
+}
+
+//ouverture portes
+void Map::openDoor() {
+    // porte principale
+    if(recTab[1].x + recTab[1].w > recTab[1].x)
+        recTab[1].w--;
+    // portes jaunes
+    for(int i=4; i<=9; i++){
+        if(recTab[i].y + recTab[i].h > recTab[i].y)
+            recTab[i].h--;
+        sleep(1);
+    }
+    //isOpen = true;
+}
+
+//fermuture portes
+void Map::closeDoor() {
+    // porte principale
+    if(recTab[1].x + recTab[1].w <= recTab[1].x)
+        recTab[1].w++;
+    // portes jaunes
+    for(int i=4; i<=9; i++){
+        if(recTab[i].h < 60)
+            recTab[i].h ++;
+    }
+    //isOpen = false;
 }
