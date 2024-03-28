28af0ec62ab5ccf4375d9a5ce77d563becdac8a7
Mamadou BAH <bah311802@gmail.com>
2024-03-28 02:01:00 +0100

    Ajout d'une liste de fleche pour le joueur

--- src/core/Player.cpp
+++ src/core/Player.cpp
@@ -12,20 +12,49 @@ Player::~Player(){
 }
 
 void Player::createArrow() {
-    Arrow b{};
-    b.arrowPos = {playerDest->x, playerDest->y};
-    b.direction = this->direction;
-    if(direction == 0)
-        b.angleRotate = -90;
-    else if(direction == 1)
-        b.angleRotate = 360;
-    else if(direction == 2)
-        b.angleRotate = 90;
-    else if(direction == 3)
-        b.angleRotate = 180;
+    // Augmentation de la zone de test sur la gauche et vers le haut
+    int playerLeft = playerDest->x - 29; // Ajout d'un décalage à gauche
+    int playerRight = playerDest->x + playerDest->w;
+    int playerTop = playerDest->y - 20; // Ajout d'un décalage vers le haut
+    int playerBottom = playerDest->y + playerDest->h;
 
-    listArrow.push_back(b);
+    // Vérifie si une flèche existe déjà dans tout le rectangle du joueur
+    bool arrowExistsInPlayerRect = false;
+    for (const auto& arrow : listArrow) {
+        // Vérifie si les coordonnées de la flèche se trouvent dans le rectangle du joueur étendu
+        if (arrow.arrowPos.x >= playerLeft && arrow.arrowPos.x <= playerRight &&
+            arrow.arrowPos.y >= playerTop && arrow.arrowPos.y <= playerBottom) {
+            arrowExistsInPlayerRect = true;
+            break;
+        }
+    }
+
+    // Si aucune flèche n'existe dans le rectangle du joueur, ajoutez une nouvelle flèche
+    if (!arrowExistsInPlayerRect) {
+        // Calcul des coordonnées du centre du joueur
+        int playerCenterX = (playerDest->x + playerDest->w / 2) - 20;
+        int playerCenterY = (playerDest->y + playerDest->h / 2) - 20;
+
+        Arrow b{};
+        b.arrowPos = {playerCenterX, playerCenterY};
+        b.direction = this->direction;
+        if (b.direction == 0) {
+            b.angleRotate = -90;
+            b.speed = -ARR_SPEED;
+        } else if (b.direction == 1) {
+            b.angleRotate = 360;
+            b.speed = +ARR_SPEED;
+        } else if (b.direction == 2) {
+            b.angleRotate = 90;
+            b.speed = +ARR_SPEED;
+        } else {
+            b.angleRotate = 180;
+            b.speed = -ARR_SPEED;
+        }
+        listArrow.push_back(b);
+    }
 }
+
 
 void Player::goLeft() const {
         playerDest->x-=1;
