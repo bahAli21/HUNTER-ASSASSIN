a03f003f24c89e3ef91d67a428b8addd0627241c
Mamadou BAH <bah311802@gmail.com>
2024-03-29 22:04:02 +0100

    Organisation du module Animation

--- src/sdl2/SDLAnimation.cpp
+++ src/sdl2/SDLAnimation.cpp
@@ -1,6 +1,6 @@
 #include "SDLAnimation.h"
 
-SDLAnimation::SDLAnimation(SDL_Renderer* renderer, const char * path, const Player& _player): player(_player),
+SDLAnimation::SDLAnimation(SDL_Renderer* renderer, const char * path, Player _player): player(_player),
 sound(44100, MIX_DEFAULT_FORMAT, 2, 248){
     allAnimation.loadSpriteFile(path, renderer);
     fireArrow.loadSpriteFile("../data/imgAnimation/fire-arrow.bmp", renderer);
@@ -11,139 +11,22 @@ sound(44100, MIX_DEFAULT_FORMAT, 2, 248){
 
 SDLAnimation::~SDLAnimation() = default;
 
-void SDLAnimation::Walk(Position tabPos[], int index) {
-    for (int i = 0; i < 8; ++i) {
-        tabPos[i].x = (i*2) * (WIDTH_A);
-        tabPos[i].y = HEIGHT_A * index;
-    }
-}
-
-void SDLAnimation::AttackInit(Position tabPos[], int index) {
-    for (int i = 0; i <=12; ++i) {
-        tabPos[i].x = (i)*(WIDTH_A);
-        tabPos[i].y = (HEIGHT_A) * index;
-    }
-}
-
-void SDLAnimation::makeAnimation(int index, Position tabPos[]) const {
-    player.playerSource->x = tabPos[index].x;
-    player.playerSource->y = tabPos[index].y;
-    player.playerSource->w = WIDTH_A;
-    player.playerDest->w = WIDTH_A;
-    player.playerSource->h = HEIGHT_A;
-    player.playerDest->h = HEIGHT_A;
-}
-
-//Je charge mon image source (pour en faire une animation video)
-void SDLAnimation::loadClips() {
-    //WALKING UP
-    Walk(player_up_clips, 21);
-    //WALKING LEFT
-    Walk(player_left_clips, 23);
-    //WALKING DOWN
-    Walk(player_down_clips, 25);
-    //WALKING RIGHT
-    Walk(player_right_clips, 27);
-    //UP
-    AttackInit(attackUp, 16);
-
-    //LEFT
-    AttackInit(attackLeft, 17);
-
-    //DOWN
-
-    AttackInit(attackDown, 18);
-
-    //RIGHT
-    AttackInit(attackRight, 19);
-
-    //Player Dead
-    AttackInit(PlayerHurtClips, 20);
-
-}
-
-//ALL ANIMATION SEQUENCE USE w , x and s
-void SDLAnimation::UP(int idxAtt, char c) {
-    if(c == 's') {
-        //Animation Attack up normal
-        makeAnimation(idxAtt,attackUp);
-    }
-}
-
-void SDLAnimation::LEFT(int idxAtt, char c) {
-    if(c == 's') {
-        //Animation Attack up normal
-        makeAnimation(idxAtt,attackLeft);
-    }
-}
-
-void SDLAnimation::DOWN(int idxAtt, char c) {
-    if(c == 's') { //shoot
-        //Animation Attack up normal
-        makeAnimation(idxAtt,attackDown);
-    }
-}
-
-void SDLAnimation::RIGHT(int idxAtt, char c) {
-    if(c == 's') {
-        //Animation Attack up normal
-        makeAnimation(idxAtt,attackRight);
-    }
-}
-
 void SDLAnimation::handleInput() {
     state = SDL_GetKeyboardState(nullptr);
 
-    moving_left = state[SDL_SCANCODE_LEFT] > 0;
-    moving_right = state[SDL_SCANCODE_RIGHT] > 0;
-    moving_up = state[SDL_SCANCODE_UP] > 0;
-    moving_down = state[SDL_SCANCODE_DOWN] > 0;
+    player.moving_left = state[SDL_SCANCODE_LEFT] > 0;
+    player.moving_right = state[SDL_SCANCODE_RIGHT] > 0;
+    player.moving_up = state[SDL_SCANCODE_UP] > 0;
+    player.moving_down = state[SDL_SCANCODE_DOWN] > 0;
 
     if(state[SDL_SCANCODE_K] > 0)
         keyBoardK = 'k';
 
     if (state[SDL_SCANCODE_S] > 0)
-        keyBoard = 's';
-
-    /*while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT) {
-            SDL_Quit();
-            exit(0);
-        }
-    }*/
+        player.shootKey = 's';
 }
 
-
-void SDLAnimation::walkingAnimation(Position tabPos[], int speed, int indexClips, int direction) {
-
-    player.playerSource->x =  tabPos[indexClips].x;
-    player.playerSource->y =  tabPos[indexClips].y;
-    player.playerSource->w = WIDTH_A*2;
-    player.playerDest->w = WIDTH_A*2;
-    player.playerSource->h = HEIGHT_A*2;
-    player.playerDest->h = HEIGHT_A*2;
-    player.direction = direction;
-    keyBoard ='_';
-    if(direction == NORTH)
-        player.playerDest->y += speed;
-    if(direction == EAST)
-        player.playerDest->x += speed;
-    if(direction == WEST)
-        player.playerDest->x += speed;
-    if(direction == SOUTH)
-        player.playerDest->y += speed;
-}
-
-void SDLAnimation::updateArrowPos() {
-    for (auto& arrow: player.listArrow) {
-        if(arrow.direction == EAST || arrow.direction == WEST)
-            arrow.arrowPos.x += arrow.speed;
-        if(arrow.direction == NORTH || arrow.direction == SOUTH)
-            arrow.arrowPos.y += arrow.speed;
-    }
-}
-
-void SDLAnimation::Arrow(SDL_Renderer * renderer) const {
+void SDLAnimation::drawArrow(SDL_Renderer * renderer) const {
 
     //Angle -90 (up) 0 ou 360 (right) 180(left) et 90(down)
     for (auto arrow: player.listArrow){
@@ -163,71 +46,47 @@ void SDLAnimation::Arrow(SDL_Renderer * renderer) const {
     }
 }
 
-void SDLAnimation::updatePlayer() {
+void SDLAnimation::updatePlayer(int dirForTest) {
     int animation_speed = SDL_GetTicks() / 170;
-    int idx = animation_speed % 7;//for walking frame
     int idxAtt= animation_speed % 13; //for animation frame
-    int  idxDead = animation_speed % 5;
-    int vitesse=2;
+    int idxDead = animation_speed % 5;
 
-    //idxAtt = 8; //Ne touche jamais gold //Maitennt on tire la fleche si idxAtt = 8
-
-
-    if (moving_up) {
-        player.direction = NORTH;
-        walkingAnimation(player_up_clips, -vitesse, idx, NORTH);
-    }
-    if (moving_left) {
-        player.direction = EAST;
-        walkingAnimation(player_left_clips, -vitesse, idx, EAST);
-    }
-    if (moving_down) {
-        player.direction = SOUTH;
-        walkingAnimation(player_down_clips, +vitesse, idx, SOUTH);
-    }
-    if (moving_right) {
-        player.direction = WEST;
-        walkingAnimation(player_right_clips, +vitesse, idx, WEST);
-    }
-
-    if(state[SDL_SCANCODE_S] > 0 && idxAtt==8) {
-        player.createArrow();
+    if(state[SDL_SCANCODE_S] > 0 && idxAtt==8 && player.canShootNow) {
+        player.createArrow(dirForTest);
         // Lecture de l'effet sonore une fois
         SDLSound::PlayChunk(shootEffect);
     }
-    updateArrowPos();
+    player.updateArrowPos();
     if (idxAtt>5)
         *ptrStop = 1;
     if(keyBoardK == 'k' && *ptrStop==0)
-         makeAnimation(idxDead, PlayerHurtClips);
+         player.makeAnimation(idxDead, PlayerHurtClips);
 
-
-        if(player.direction == NORTH) {
-            UP(idxAtt,keyBoard);
+        if(dirForTest == NORTH && player.canShootNow) {
+            player.UP(idxAtt);
         }
 
-        if(player.direction == EAST) {
-            LEFT(idxAtt,keyBoard);
+        if(dirForTest == EAST && player.canShootNow) {
+            player.LEFT(idxAtt);
         }
 
-        if(player.direction == SOUTH) {
-            DOWN(idxAtt,keyBoard);
+        if(dirForTest == SOUTH && player.canShootNow) {
+            player.DOWN(idxAtt);
         }
-        if(player.direction == WEST) {
-            RIGHT(idxAtt,keyBoard);
+        if(dirForTest == WEST && player.canShootNow) {
+            player.RIGHT(idxAtt);
         }
-     keyBoard = ' '; //don't repeat animation
+    player.shootKey = ' '; //don't repeat animation
 
 }
 
 void SDLAnimation::DrawAnimation(SDL_Renderer * renderer) const {
 
-    Arrow(renderer);
+    drawArrow(renderer);
     SDL_RenderCopy(renderer,
                    allAnimation._texture,
                    reinterpret_cast<const SDL_Rect *>(player.playerSource),
                    reinterpret_cast<const SDL_Rect *>(player.playerDest));
-
 
 }
 
--- src/sdl2/SDLAnimation.h
+++ src/sdl2/SDLAnimation.h
@@ -26,22 +26,6 @@ public:
     Mix_Chunk * shootEffect;
     /** Pour les autre sprite aussi */
     SDLSprite allAnimation; /**< Sprite object containing all animations. */
-    Position player_left_clips[9]{}; /**< Array of positions for left movement animation. */
-    Position player_right_clips[9]{}; /**< Array of positions for right movement animation. */
-    Position player_up_clips[9]{}; /**< Array of positions for up movement animation. */
-    Position player_down_clips[9]{}; /**< Array of positions for down movement animation. */
-    Position attackUp[13]{}; /**< Array of positions for attacking upwards animation. */
-    Position attackUpReverse[7]{}; /**< Array of positions for reverse attacking upwards animation. */
-    Position attackLeft[13]{}; /**< Array of positions for attacking left animation. */
-    Position attackLeftReverse[7]{}; /**< Array of positions for reverse attacking left animation. */
-    Position attackDown[13]{}; /**< Array of positions for attacking downwards animation. */
-    Position attackDownReverse[7]{}; /**< Array of positions for reverse attacking downwards animation. */
-    Position attackRight[13]{}; /**< Array of positions for attacking right animation. */
-    Position attackRightReverse[7]{}; /**< Array of positions for reverse attacking right animation. */
-    Position attackUpStabbed[7]{}; /**< Array of positions for attacking upwards with stabbing animation. */
-    Position attackLeftStabbed[7]{}; /**< Array of positions for attacking left with stabbing animation. */
-    Position attackDownStabbed[7]{}; /**< Array of positions for attacking downwards with stabbing animation. */
-    Position attackRightStabbed[7]{}; /**< Array of positions for attacking right with stabbing animation. */
     Position PlayerHurtClips[7];
     /**
      * @brief Constructor for SDLAnimation class.
@@ -49,46 +33,14 @@ public:
      * @param path Path to sprite image.
      * @param _player Player object.
      */
-    SDLAnimation(SDL_Renderer* renderer, const char * path, const Player& _player);
+    SDLAnimation(SDL_Renderer* renderer, const char * path,  Player _player);
     const Uint8* state;
-    void updateArrowPos();
     /**
      * @brief Destructor for SDLAnimation class.
      */
     ~SDLAnimation();
-    void Arrow(SDL_Renderer * renderer) const;
-    char keyBoard{}; /**< Key pressed by player. */
-    char keyBoardD{}; /**< Key pressed by player. */
+
     char keyBoardK{}; /**< Key pressed by player. */
-    bool isDraw = false;
-    bool moving_left{}; /**< Flag indicating left movement. */
-    bool moving_right{}; /**< Flag indicating right movement. */
-    bool moving_up{}; /**< Flag indicating up movement. */
-    bool moving_down{}; /**< Flag indicating down movement. */
-
-    /**
-     * @brief Initiates walking animation.
-     */
-    void Walk(Position tabPos[], int index);
-
-    /**
-     * @brief Initializes attack animation.
-     * @param tabPos Array of positions for animation.
-     * @param index Index of animation.
-     */
-    static void AttackInit(Position tabPos[], int index);
-
-    /**
-     * @brief Creates animation frames.
-     * @param index Index of animation.
-     * @param tabPos Array of positions for animation.
-     */
-    void makeAnimation(int index, Position tabPos[]) const;
-
-    /**
-     * @brief Loads animation clips.
-     */
-    void loadClips();
 
     /**
      * @brief Handles input from keyboard.
@@ -96,18 +48,9 @@ public:
     void handleInput();
 
     /**
-     * @brief Performs walking animation.
-     * @param tabPos Array of positions for animation.
-     * @param speed Animation speed.
-     * @param indexClips Index of animation clip.
-     * @param direction Direction of movement.
-     */
-    void walkingAnimation(Position tabPos[], int speed, int indexClips, int direction);
-
-    /**
      * @brief Updates player state.
      */
-    void updatePlayer();
+     void updatePlayer(int dirForTest);
 
     /**
      * @brief Draws animation on renderer.
@@ -115,34 +58,7 @@ public:
      */
     void DrawAnimation(SDL_Renderer * renderer) const;
 
-    /**
- * @brief Handles movement animation for moving upwards.
- * @param idxAtt Index of the attack animation.
- * @param c Character representing the direction of animation.
- */
-    void UP(int idxAtt, char c);
-
-    /**
-     * @brief Handles movement animation for moving leftwards.
-     * @param idxAtt Index of the attack animation.
-     * @param c Character representing the direction of animation.
-     */
-    void LEFT(int idxAtt, char c);
-
-    /**
-     * @brief Handles movement animation for moving downwards.
-     * @param idxAtt Index of the attack animation.
-     * @param c Character representing the direction of animation.
-     */
-    void DOWN(int idxAtt, char c);
-
-    /**
-     * @brief Handles movement animation for moving rightwards.
-     * @param idxAtt Index of the attack animation.
-     * @param c Character representing the direction of animation.
-     */
-    void RIGHT(int idxAtt, char c);
-
+    void drawArrow(SDL_Renderer * renderer) const;
 };
 
 #endif //HUNTERASSASSIN_SDLANIMATION_H
