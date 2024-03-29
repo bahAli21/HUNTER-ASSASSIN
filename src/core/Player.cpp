cad8bb6bd5271623c3885d2bcab4c6cb2ad016b0
Mamadou BAH <bah311802@gmail.com>
2024-03-29 22:06:55 +0100

    Plus d'amelioration du module Player

--- src/core/Player.cpp
+++ src/core/Player.cpp
@@ -3,19 +3,129 @@
 #include "Player.h"
 Player::Player() :health(100){
     playerDest = new Rect{WINDOW_W/2-70,WINDOW_H-PLAYER_HEIGHT*4, PLAYER_WIDTH, PLAYER_HEIGHT*2};
-    playerSource = new Rect{PLAYER_WIDTH/2,PLAYER_HEIGHT*21,PLAYER_WIDTH,PLAYER_HEIGHT*2};
+    playerSource = new Rect{PLAYER_WIDTH/2,PLAYER_HEIGHT*27,PLAYER_WIDTH,PLAYER_HEIGHT*2};
     dest = new Rect{WINDOW_W/2-70,WINDOW_H-PLAYER_HEIGHT*4, PLAYER_WIDTH, PLAYER_HEIGHT};
+    loadClips();
+    //this->direction = 1;
+    //shootKey = 's';
 }
 
 Player::~Player(){
     health=0;
 }
 
-void Player::createArrow() {
+void Player::Walk(Position *tabPos, int index) {
+    for (int i = 0; i < 8; ++i) {
+        tabPos[i].x = (i*2) * (WIDTH_A);
+        tabPos[i].y = HEIGHT_A * index;
+    }
+}
+
+void Player::AttackInit(Position *tabPos, int index) {
+    for (int i = 0; i <=12; ++i) {
+        tabPos[i].x = (i)*(WIDTH_A);
+        tabPos[i].y = (HEIGHT_A) * index;
+    }
+}
+
+void Player::makeAnimation(int index, Position *tabPos) {
+    playerSource->x = tabPos[index].x;
+    playerSource->y = tabPos[index].y;
+    playerSource->w = WIDTH_A;
+    playerDest->w = WIDTH_A;
+    playerSource->h = HEIGHT_A;
+    playerDest->h = HEIGHT_A;
+}
+
+
+void Player::WalkingAnimation(Position *tabPos, int speed, int indexClips, int direction) {
+    playerSource->x =  tabPos[indexClips].x;
+    playerSource->y =  tabPos[indexClips].y;
+    playerSource->w = WIDTH_A * 2; // Multiplication par 2 pour l'animation de marche
+    playerSource->h = HEIGHT_A * 2;
+
+    // Réinitialisation de la taille du rectangle de destination
+    playerDest->w = WIDTH_A*2;
+    playerDest->h = HEIGHT_A*2;
+
+    // Ajustement de la position en fonction de la direction
+    if(direction == 0 || direction == 2) // Haut ou Bas
+        playerDest->y += speed / 2; // Division par 2 pour compenser la multiplication par 2
+    else if(direction == 1 || direction == 3) // Gauche ou Droite
+        playerDest->x += speed;
+
+}
+
+
+//ALL ANIMATION SEQUENCE USE w , x and s
+void Player::UP(int idxAtt) {
+    if(shootKey == 's') {
+        //Animation Attack up normal
+        makeAnimation(idxAtt,attackUp);
+    }
+}
+
+void Player::LEFT(int idxAtt) {
+    if(shootKey == 's') {
+        //Animation Attack up normal
+        makeAnimation(idxAtt,attackLeft);
+    }
+}
+
+void Player::DOWN(int idxAtt) {
+    if(shootKey == 's') { //shoot
+        //Animation Attack up normal
+        makeAnimation(idxAtt,attackDown);
+    }
+}
+
+void Player::RIGHT(int idxAtt) {
+    if(shootKey == 's') {
+        //Animation Attack up normal
+        makeAnimation(idxAtt,attackRight);
+    }
+}
+
+void Player::updateArrowPos() {
+    for (auto& arrow: listArrow) {
+        if(arrow.direction == WEST || arrow.direction == EAST)
+            arrow.arrowPos.x += arrow.speed;
+        if(arrow.direction == NORTH || arrow.direction == SOUTH)
+            arrow.arrowPos.y += arrow.speed;
+    }
+}
+
+void Player::loadClips() {
+    //WALKING UP
+    Walk(player_up_clips, 21);
+    //WALKING LEFT
+    Walk(player_left_clips, 23);
+    //WALKING DOWN
+    Walk(player_down_clips, 25);
+    //WALKING RIGHT
+    Walk(player_right_clips, 27);
+    //UP
+    AttackInit(attackUp, 16);
+
+    //LEFT
+    AttackInit(attackLeft, 17);
+
+    //DOWN
+
+    AttackInit(attackDown, 18);
+
+    //RIGHT
+    AttackInit(attackRight, 19);
+
+    //Player Dead
+    AttackInit(PlayerHurtClips, 20);
+}
+
+void Player::createArrow(int direct) {
     // Augmentation de la zone de test sur la gauche et vers le haut
     int playerLeft = playerDest->x - 29; // Ajout d'un décalage à gauche
     int playerRight = playerDest->x + playerDest->w;
-    int playerTop = playerDest->y - 20; // Ajout d'un décalage vers le haut
+    int playerTop = playerDest->y - 30; // Ajout d'un décalage vers le haut
     int playerBottom = playerDest->y + playerDest->h;
 
     // Vérifie si une flèche existe déjà dans tout le rectangle du joueur
@@ -33,25 +143,25 @@ void Player::createArrow() {
     if (!arrowExistsInPlayerRect) {
         // Calcul des coordonnées du centre du joueur
         int playerCenterX = (playerDest->x + playerDest->w / 2) - 20;
-        int playerCenterY = (playerDest->y + playerDest->h / 2) - 20;
+        int playerCenterY = (playerDest->y + playerDest->h / 2) - 25;
 
-        Arrow b{};
-        b.arrowPos = {playerCenterX, playerCenterY};
-        b.direction = this->direction;
-        if (b.direction == 0) {
-            b.angleRotate = -90;
-            b.speed = -ARR_SPEED;
-        } else if (b.direction == 1) {
-            b.angleRotate = 360;
-            b.speed = +ARR_SPEED;
-        } else if (b.direction == 2) {
-            b.angleRotate = 90;
-            b.speed = +ARR_SPEED;
+        Arrow arrow{};
+        arrow.arrowPos = {playerCenterX, playerCenterY};
+        arrow.direction = direct;
+        if (arrow.direction == 0) {
+            arrow.angleRotate = -90;
+            arrow.speed = -ARR_SPEED;
+        } else if (arrow.direction == 1) {
+            arrow.angleRotate = 360;
+            arrow.speed = +ARR_SPEED;
+        } else if (arrow.direction == 2) {
+            arrow.angleRotate = 90;
+            arrow.speed = +ARR_SPEED;
         } else {
-            b.angleRotate = 180;
-            b.speed = -ARR_SPEED;
+            arrow.angleRotate = 180;
+            arrow.speed = -ARR_SPEED;
         }
-        listArrow.push_back(b);
+        listArrow.push_back(arrow);
     }
 }
 
--- src/core/Player.h
+++ src/core/Player.h
@@ -9,6 +9,7 @@
 #include <iostream>
 #include "vector"
 #include "Arrow.h"
+#include "Direction.h"
 
 /**
  * @brief Class representing the player in the game.
@@ -21,15 +22,40 @@ public:
     Rect * playerDest; ///< Destination of the player.
     Rect * playerSource; ///< Source of the player.
     Rect * dest; /**< Destination of the AI. */
-    int direction; /**< Direction indicators of the player.*/
+    int direction{}; /**< Direction indicators of the player.*/
     std::vector<Arrow> listArrow; /**< Liste des tuiles du niveau. */
+    bool moving_left{}; /**< Flag indicating left movement. */
+    bool moving_right{}; /**< Flag indicating right movement. */
+    bool moving_up{}; /**< Flag indicating up movement. */
+    bool moving_down{}; /**< Flag indicating down movement. */
+    Position player_left_clips[9]{}; /**< Array of positions for left movement animation. */
+    Position player_right_clips[9]{}; /**< Array of positions for right movement animation. */
+    Position player_up_clips[9]{}; /**< Array of positions for up movement animation. */
+    Position player_down_clips[9]{}; /**< Array of positions for down movement animation. */
+    Position attackUp[13]{}; /**< Array of positions for attacking upwards animation. */
+    Position attackLeft[13]{}; /**< Array of positions for attacking left animation. */
+    Position attackDown[13]{}; /**< Array of positions for attacking downwards animation. */
+    Position attackRight[13]{}; /**< Array of positions for attacking right animation. */
+    Position PlayerHurtClips[7];
+    void AttackInit(Position tabPos[], int index);
+    void makeAnimation(int index, Position tabPos[]);
+    void WalkingAnimation(Position tabPos[], int speed, int indexClips, int direction);
+    void UP(int idxAtt);
+    void LEFT(int idxAtt);
+    void RIGHT(int idxAtt);
+    void DOWN(int idxAtt);
+    void updateArrowPos();
+    char shootKey{} ;
+    bool canShootNow = false;
 
+    void Walk(Position tabPos[], int index);
+    void loadClips();
 public:
     /**
      * @brief Moves the player to the left on the specified map.
      * @param map The map on which the player moves.
      */
-     void createArrow();
+     void createArrow(int direct);
     void goLeft() const;
 
     /**
